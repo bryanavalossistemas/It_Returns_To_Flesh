6adce4a73e832d4f7dfc71881f6db1d5302fe416
I21318 <I21318@lab.aca>
2026-05-29 16:43:26 -0500

    d

--- Assets/Game/Scripts/DeathZone.cs
+++ Assets/Game/Scripts/DeathZone.cs
@@ -4,11 +4,17 @@ public class DeathZone : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D col)
     {
-        FleshRipper ripper = col.GetComponent<FleshRipper>();
+        // Usamos GetComponentInParent por si el collider que choca está en un objeto hijo
+        FleshRipper ripper = col.GetComponentInParent<FleshRipper>();
 
         if (ripper != null)
         {
+            Debug.Log("Ripper destroyed");
             Destroy(ripper.gameObject);
+        }
+        else
+        {
+            Debug.Log("Algo tocó la DeathZone, pero no es un FleshRipper: " + col.name);
         }
     }
 }
