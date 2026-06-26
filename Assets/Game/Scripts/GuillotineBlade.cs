64f5d32eaae3b573e638d4b6741f790f81e17d25
AdriUlima <20200711@aloe.ulima.edu.pe>
2026-06-26 18:17:02 -0500

    test

---
+++ Assets/Game/Scripts/GuillotineBlade.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class GuillotineBlade : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("TRIGGER TOCADO por: " + other.name + " | Layer: " + LayerMask.LayerToName(other.gameObject.layer));
+
+        FleshRipper ripper = other.GetComponent<FleshRipper>()
+                          ?? other.GetComponentInParent<FleshRipper>();
+
+        if (ripper != null)
+        {
+            Debug.Log("Ripper encontrado → muriendo");
+            ripper.RipperDead();
+        }
+        else
+        {
+            Debug.Log("NO se encontró FleshRipper en: " + other.name);
+        }
+    }
+}
